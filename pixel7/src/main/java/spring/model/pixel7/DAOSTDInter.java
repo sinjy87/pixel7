62271cd7764ab8a2b302b5b8f88a1b266fca44ac
soldesk <soldesk@s4>
2017-03-10 16:27:36 +0900

    Junit 테스트 완료

--- pixel7/src/main/java/spring/model/pixel7/DAOSTDInter.java
+++ pixel7/src/main/java/spring/model/pixel7/DAOSTDInter.java
@@ -6,9 +6,9 @@ import java.util.Map;
 public interface DAOSTDInter {
 public boolean create(Object dto)throws Exception;
 public List list(Map map)throws Exception;
-public Object read(Object img_num)throws Exception;
+public Object read(Object pk)throws Exception;
 public boolean update(Object dto)throws Exception;
-public boolean delete(Object img_num)throws Exception;
+public boolean delete(Object pk)throws Exception;
 public int total(Map map)throws Exception;
  
 }
